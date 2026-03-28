69fbf46115d8729ec6b2b7b25c20c01ea490d92a
Nilan Hansajith <155352473+HansajithNilan@users.noreply.github.com>
2026-03-28 11:11:57 +0530

    Delete src/main directory

--- src/main/java/com/example/courseservice/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.courseservice.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
